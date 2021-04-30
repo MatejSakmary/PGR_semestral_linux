a6f9ba90da4e95770f9e3cffcf59fabbf6838874
MatějSakmary <matej.hydro@gmail.com>
2021-04-30 19:29:16 +0200

    finished Mesh class

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -36,3 +36,34 @@ void Mesh::setupMesh()
 
     glBindVertexArray(0);
 }
+
+void Mesh::Draw(Shader &shader)
+{
+    unsigned int diffuseNum = 1;
+    unsigned int specularNum = 1;
+
+    /* go through all the loaded textures*/
+    for (unsigned int i = 0; i < textures.size(); i++)
+    {
+        /* activate each texture GL_TEXTURE0 + n = GL_TEXTUREN*/
+        glActiveTexture(GL_TEXTURE0 + i);
+
+        std::string number;
+        std::string name = textures[i].type;
+        if (name == "texture_diffuse")
+        {
+            number = std::to_string(diffuseNum++);
+        }else if (name == "texture_specular")
+        {
+            number = std::to_string(specularNum++);
+        }
+        // this sets texture sampler with name and corresponding number in the shader
+        shader.setFloat(("material."+ name + number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+    glActiveTexture(GL_TEXTURE0);
+    
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
